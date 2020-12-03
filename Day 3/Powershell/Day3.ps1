a685e121d8e18c7d3d2e6409e76b265954786058
Jim <James.lee.walters@gmail.com>
2020-12-03 18:15:47 +0000

    Day 3 Powershell Part 2 added

--- Day 3/Powershell/Day3.ps1
+++ Day 3/Powershell/Day3.ps1
@@ -1,21 +1,51 @@
 $SkiSlope = Get-Content $PSScriptRoot\input.txt
-$SkiSlope[0]
+#Part 1
 $i = 0
-$y = 0
+$j = 0
 $trees = 0
 $down = 1
 $right = 3
 Do{
   $i = $i + $down
-  if($y + $right -gt $skislope[0].length -1){
-      $y = ($y + $right) - $skislope[0].Length
+  if($j + $right -gt $skislope[0].length -1){
+      $j = ($j + $right) - $skislope[0].Length
   }
   else{
-    $y = $y + $right  
+    $j = $j + $right  
   }
-  if($SkiSlope[$i][$y] -eq "#"){
+  if($SkiSlope[$i][$j] -eq "#"){
       $trees++
   }
 }
 While ($i -lt $SkiSlope.count -1)
 Write-Host Trees: $trees
+#Part 2
+$Totaltrees = 1
+$Vectors =
+@(@{x=1;y=1},
+@{x=3;y=1},
+@{x=5;y=1},
+@{x=7;y=1},
+@{x=1;y=2}
+)
+ForEach($vector in $vectors) {
+    $i = 0
+    $j = 0
+    $trees = 0
+    Do{
+        $i = $i + $vector.y
+        if($j + $vector.x -gt $skislope[0].length -1){
+            $j = ($j + $vector.x) - $skislope[0].Length
+        }
+        else{
+          $j = $j + $vector.x  
+        }
+        if($SkiSlope[$i][$j] -eq "#"){
+            $trees++
+        }
+      }
+      While ($i -lt $SkiSlope.count -1)
+      $TotalTrees = $Totaltrees * $trees
+
+}
+Write-Host "Trees Part 2: " $TotalTrees

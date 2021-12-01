a4bade76d8cba14f10932cdb25d1a7ca9adcef6e
VampyrByte <james.lee.walters@gmail.com>
2021-12-01 19:07:28 +0000

    add day1 part 2

---
+++ 2021/Day 1/Day1p2.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var lines []int
+	file, _ := os.Open("./input")
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line, _ := strconv.Atoi(scanner.Text())
+		lines = append(lines, line)
+	}
+	counter := 0
+	for i := 0; i < len(lines)-3; i++ {
+		lowerWindowSum := lines[i] + lines[i+1] + lines[i+2]
+		higherWindowSum := lines[i+1] + lines[i+2] + lines[i+3]
+		if higherWindowSum > lowerWindowSum {
+			counter++
+		}
+
+	}
+	fmt.Println(counter)
+
+}
